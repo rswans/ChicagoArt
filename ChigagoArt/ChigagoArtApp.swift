9ec8435421d62d4ebb86f8d264b0d350d81ae0b9
Rick Swanson <swasn.rick@gmail.com>
2026-05-31 14:54:31 +0100

    Initial Commit

---
+++ ChigagoArt/ChigagoArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChigagoArtApp.swift
+//  ChigagoArt
+//
+//  Created by Richard Swanson on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChigagoArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
